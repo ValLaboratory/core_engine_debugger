fbcc1f526b3f5f5d54aad5ba3a7dce78c8ba3c0f
VAL_Shiohata <shiohata@val.co.jp>
2026-01-07 14:16:51 +0900

    Shift-JIS からUTF-8 への変換関数を追加

--- common.c
+++ common.c
@@ -1,4 +1,8 @@
+#include <iconv.h>
+
 #include "common.h"
+
+#define MAX_BUFFER_SIZE 1024
 
 /**
  * @brief   ログメッセージをコンソールへ表示
@@ -19,4 +23,31 @@ void stdout_message(const char* sz_category, const char* sz_message) {
     strftime(sz_timestamp, sizeof(sz_timestamp), "%Y-%m-%d %H:%M:%S", st_local);
 
     printf("%s: [%s] %s\n", sz_timestamp, sz_category, sz_message);
+}
+
+/**
+ * @brief   Shift-JIS(CP932) からUTF-8 へ文字コードを変換
+ * @param   const char* : 変換対象の文字列
+ * @return  char*
+ *
+ * @copyright   val lab corp.
+ * @author      shiohata
+ * @data        2026-01-07  新規作成
+*/
+void convert_sjis2utf8(char* sz_src, char* sz_dst) {
+    iconv_t ic = iconv_open("utf8", "cp932");
+    if(ic == (iconv_t)-1) {
+        stdout_message("ERROR", "failed to initialize iconv.");
+        return;
+    }
+
+    char*   sz_src_buf  = sz_src;
+    char*   sz_dst_buf  = sz_dst;
+    size_t  src_size    = (size_t)MAX_BUFFER_SIZE;
+    size_t  dst_size    = (size_t)MAX_BUFFER_SIZE;
+    if(iconv(ic, &sz_src_buf, &src_size, &sz_dst_buf, &dst_size) == -1) {
+        stdout_message("ERROR", "failed to convert strings.");
+        return;
+    }
+    iconv_close(ic);
 }
--- common.h
+++ common.h
@@ -6,3 +6,6 @@
 
 /* ログを出力 */
 void stdout_message(const char* sz_category, const char* sz_message);
+
+/* Shift-JIS(CP932) からUTF-8 へ文字列を変換*/
+void convert_sjis2utf8(char *sz_str, char *sz_dst);
